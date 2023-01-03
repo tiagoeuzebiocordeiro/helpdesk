c87fb57f809fcdc231113a04e6e653e528c84d31
tiagoeuzebiocordeiro <tiago.ec2709@gmail.com>
2023-01-03 13:50:52 -0300

    Classes de dominio - parte 2 (Anotacoes, mapeamento)

--- src/main/java/com/tiago/helpdesk/domain/Cliente.java
+++ src/main/java/com/tiago/helpdesk/domain/Cliente.java
@@ -3,16 +3,26 @@ package com.tiago.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+import com.tiago.helpdesk.domain.enums.Perfil;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
