1b8392880a1331aab97cee6cee71a7cfe958574e
tiagoeuzebiocordeiro <tiago.ec2709@gmail.com>
2023-01-04 11:26:23 -0300

    Padrao DTO para Tecnico

--- src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiago.helpdesk.domain.Tecnico;
+import com.tiago.helpdesk.domain.dtos.TecnicoDTO;
 import com.tiago.helpdesk.services.TecnicoService;
 
 @RestController
@@ -25,9 +26,9 @@ public class TecnicoResource {
 	private TecnicoService tecnicoService;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = tecnicoService.findById(id);
-		return ResponseEntity.ok(obj); // Retorna o corpo do objeto como resposta
+		return ResponseEntity.ok(new TecnicoDTO(obj)); 
 	}
 	
 }
