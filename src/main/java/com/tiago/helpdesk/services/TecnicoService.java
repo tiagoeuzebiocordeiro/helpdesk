44615e303b1d064dbbd5bf4c394408ccc1016a68
tiagoeuzebiocordeiro <tiago.ec2709@gmail.com>
2023-01-04 14:26:26 -0300

    Endpoint findAll para Tecnico

--- src/main/java/com/tiago/helpdesk/services/TecnicoService.java
+++ src/main/java/com/tiago/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.tiago.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class TecnicoService {
 		 * no banco.*/
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. ID: " + id)); // Exceção arrow function
+	}
+	
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 	
 }
