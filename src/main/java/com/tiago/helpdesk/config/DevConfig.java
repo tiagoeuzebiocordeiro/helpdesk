69080fcda67e1eb56dbf0b598f6421ebef92481e
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2023-09-27 10:35:54 -0300

    Perfil de desenvolvimento

--- src/main/java/com/tiago/helpdesk/config/DevConfig.java
+++ src/main/java/com/tiago/helpdesk/config/DevConfig.java
@@ -1,7 +1,12 @@
 package com.tiago.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.tiago.helpdesk.services.DBService;
 
 /*Toda vida que esse perfil "test" estiver ativo, ele vai subir essa BEAN (o método) de forma automática
  * ou seja, o método tá chamando o serviço que tem a lógica de popular a base de dados
@@ -9,9 +14,21 @@ import org.springframework.context.annotation.Profile;
  * eu informei no app properties que o perfil era o test.*/
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
 
+    @Bean
+    public boolean instanciaDb() {
+    	if (value.equals("create")) {
+    		dbService.instanciaDb();
+    	}
+    	return false;
+	}
 	
 }
