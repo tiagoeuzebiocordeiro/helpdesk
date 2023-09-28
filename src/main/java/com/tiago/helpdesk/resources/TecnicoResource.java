460c3b9c5e47f5924095c0f0665b81a2aef693ad
tiagoeuzebiocordeiro <tiagoezc@hotmail.com>
2023-09-28 14:59:01 -0300

    Changes TecnicoResource + TecnicoDTO

--- src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/tiago/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiago.helpdesk.domain.Tecnico;
+import com.tiago.helpdesk.domain.dtos.TecnicoDTO;
 import com.tiago.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService tecnicoService;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = this.tecnicoService.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 	
 }
